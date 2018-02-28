01d1edaad101b65158190db311c463354ab133fc
saxgutierrez <diegofdog@gmail.com>
2018-02-27 21:35:47 -0500

    agregando la ruta general

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  resources :products
+  #get 'products/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
