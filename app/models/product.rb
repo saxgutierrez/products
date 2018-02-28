edc2bcf511082980844f73caa94a0d840435791b
saxgutierrez <diegofdog@gmail.com>
2018-02-28 15:25:18 -0500

    agregando validaciones

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
 	has_and_belongs_to_many :categories
+	validates :name, presence: true, on: :create
+	validates :price, presence: true, numericality: true, on: :create
 end
