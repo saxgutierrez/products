d8be304a233d952176a754c381eb7631df692ce0
saxgutierrez <diegofdog@gmail.com>
2018-03-01 20:30:07 -0500

    anotaciones en consola comentadas, valores por defecto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(name:Peinilla ,price: "1000")
+Product.create(name:"Peinilla" ,price: 1000)
 
-Category.create(name:Perfumes)
-Category.create(name:Libros)
-Category.create(name:Tecnología)
-Category.create(name:Aseo)
-Category.create(name:Juguetes)
-Category.create(name:ropa)
-Category.create(name:Oficina)
-Category.create(name:Mascotas)
+Category.create(name:"Perfumes")
+Category.create(name:"Libros")
+Category.create(name:"Tecnología")
+Category.create(name:"Aseo")
+Category.create(name:"Juguetes")
+Category.create(name:"ropa")
+Category.create(name:"Oficina")
+Category.create(name:"Mascotas")
+
+
+
+#ejemplo de consola para modelos
+#product1 = Product.first
+#category1 = Category.first
+#category2 = Category.find(4)
+#category3 = Category.find(6)
+#product1.categories << category1
+#product1.categories << category2
+#product1.categories << category3
+
+#ejemplo de consola para tabla intermedia o join table
+#product1 = Product.create(name:"Peinilla" ,price: 1000)
+#product1.category_ids = [4,6]
+#product1.categories
